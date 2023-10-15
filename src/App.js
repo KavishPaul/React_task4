d0236a970015c6c6cf39f1413a33b3a7c96fff0e
KavishPaul <133625640+KavishPaul@users.noreply.github.com>
2023-10-16 00:04:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Datausers from './components/Datausers';
+
+function App() {
+  return (
+    <div className="App">
+      <Datausers />
+    </div>
+  );
+}
+
+export default App;
